718b5212d9f628e01abd10f37587bcaa23fb4c54
OzanOcak <ozanocak@gmail.com>
2024-12-14 13:43:27 -0500

    initial set up and toggle

---
+++ components/theme-provider.tsx
@@ -0,0 +1,10 @@
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+// yarn add nect-thems for ThemeProvider
